69a889a523818364e517d9fd40586a5ff976d3d6
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-06-12 01:27:25 +0530

    Time: 0 ms (100.00%), Space: 8.8 MB (58.78%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,28 +1,42 @@
 class Solution {
-    
 public:
-    void soln(vector<int>& nums, int n, int i, vector<vector<int>> &ans, vector<int> temp, set<vector<int>> & mp){
-        if(i==n){
-            if(mp.find(temp)==mp.end()){
-                ans.push_back(temp);
-                mp.insert(temp);
-            }
-                
-            return;
-        }
-        
-        soln(nums, n, i+1, ans, temp, mp);
-        // if(i>0 && nums[i]!=nums[i-1])
-         temp.push_back(nums[i]);
-       
-        soln(nums, n, i+1, ans, temp, mp);
-    }
+    
+//  vector<vector<int> > subsetsWithDup(vector<int> &S) {
+//         vector<vector<int> > totalset = {{}};
+//         sort(S.begin(),S.end());
+//         for(int i=0; i<S.size();){
+//             int count = 0; // num of elements are the same
+//             while(count + i<S.size() && S[count+i]==S[i])  count++;
+//             int previousN = totalset.size();
+//             for(int k=0; k<previousN; k++){
+//                 vector<int> instance = totalset[k];
+//                 for(int j=0; j<count; j++){
+//                     instance.push_back(S[i]);
+//                     totalset.push_back(instance);
+//                 }
+//             }
+//             i += count;
+//         }
+//         return totalset;
+//         }
+    
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+         vector<vector<int>> soln = {{}};
         sort(nums.begin(), nums.end());
-        vector<vector<int>> ans;
-        set<vector<int>> m;
-        vector<int> temp;
-        soln(nums, nums.size(), 0, ans, temp, m);
-        return ans;
+      
+        for(int i=0;i<nums.size();){
+            int c=0;
+            while(c+i<nums.size() && nums[c+i]== nums[i] )c++;
+            int k = soln.size();
+            for(int l=0;l<k;l++){
+                vector<int> temp = soln[l];
+                for(int a =0;a<c;a++){
+                    temp.push_back(nums[i]);
+                    soln.push_back(temp);
+                }
+            }
+            i+=c;
+        }
+        return soln;
     }
 };
