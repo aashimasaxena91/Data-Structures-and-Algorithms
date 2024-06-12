64d7f9681341234b352745318c3403ca14d904cf
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-06-13 01:54:42 +0530

    Time: 0 ms (100.00%), Space: 8.7 MB (66.53%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,42 +1,26 @@
 class Solution {
 public:
     
-//  vector<vector<int> > subsetsWithDup(vector<int> &S) {
-//         vector<vector<int> > totalset = {{}};
-//         sort(S.begin(),S.end());
-//         for(int i=0; i<S.size();){
-//             int count = 0; // num of elements are the same
-//             while(count + i<S.size() && S[count+i]==S[i])  count++;
-//             int previousN = totalset.size();
-//             for(int k=0; k<previousN; k++){
-//                 vector<int> instance = totalset[k];
-//                 for(int j=0; j<count; j++){
-//                     instance.push_back(S[i]);
-//                     totalset.push_back(instance);
-//                 }
-//             }
-//             i += count;
-//         }
-//         return totalset;
-//         }
     
-    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-         vector<vector<int>> soln = {{}};
-        sort(nums.begin(), nums.end());
-      
-        for(int i=0;i<nums.size();){
-            int c=0;
-            while(c+i<nums.size() && nums[c+i]== nums[i] )c++;
-            int k = soln.size();
-            for(int l=0;l<k;l++){
-                vector<int> temp = soln[l];
-                for(int a =0;a<c;a++){
-                    temp.push_back(nums[i]);
-                    soln.push_back(temp);
-                }
-            }
-            i+=c;
+    void soln(vector<int>& nums, int n, vector<vector<int>> & res, vector<int>& temp, int i){
+        if(i>=n){
+            res.push_back(temp);
+            return;
         }
-        return soln;
+        
+        temp.push_back(nums[i]);
+        soln(nums,n, res, temp, i+1);
+        temp.pop_back();
+        i++;
+        while(i<n && nums[i]==nums[i-1]) i++;
+        soln(nums,n, res, temp, i);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+         sort(nums.begin(), nums.end());
+        vector<vector<int>> res ;
+        vector<int> temp;
+        soln(nums, nums.size(), res, temp, 0);
+       
+        return res;
     }
 };
