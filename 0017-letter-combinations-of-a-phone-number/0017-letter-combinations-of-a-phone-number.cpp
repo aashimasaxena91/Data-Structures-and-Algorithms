cf21b2a8ec86ecb016b3b3f8e1cb1ebe4556a884
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-06-14 00:45:41 +0530

    Time: 0 ms (100.00%), Space: 10.3 MB (5.38%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     
-    void soln(string digits, int n, map<char, vector<char>> mp, string temp, vector<string>& res, int i ){
+    void soln(string digits, int n, unordered_map<char, vector<char>> mp, string temp, vector<string>& res, int i ){
         
         if(i>=n){
             res.push_back(temp);
@@ -17,7 +17,7 @@ public:
         int n = digits.length();
         vector<string> res;
         if(n==0) return res;
-        map<char, vector<char>> mp;
+        unordered_map<char, vector<char>> mp;
         int k=0;
         int t = 3;
         char two = '0';
@@ -29,12 +29,9 @@ public:
                 k++;
             }
         }
-        // string temp=digits;
-      
-        string temp = digits;
-        // for(int i = 0;i<n;i++){
-        //     temp+=mp[digits[i]][0];
-        // }
+         string temp = digits;
+       
+
          soln( digits, n, mp, temp, res, 0 );
      
         return res;
