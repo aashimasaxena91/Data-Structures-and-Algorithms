23932022dc05f6cde7e7812664c779d78fdb4fbf
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-06-17 21:31:05 +0530

    Time: 3 ms (68.00%), Space: 6.9 MB (99.60%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     bool judgeSquareSum(int c) {
-      //   if(c<=1) return true;
-      // int l=0, r = c/2;
-      //   while(l<=r){
-      //       if((l!=0 &&l>INT_MAX/l) ||( r!=0 && r>INT_MAX/r)) return false;
-      //        int mid = l*l + r*r;
-      //       if(mid == c) return true;
-      //       else if(mid<c) l++;
-      //       else r--;
-      //   }
-      //   return false;
-
-        int a = sqrt(c);
-        for(long i=0;i*i<= c;i++){
-            double b = sqrt(c - i*i);
-            if(b == (int)b ) return true;
+        if(c<=1) return true;
+      long l=0, r = sqrt(c);
+        while(l<=r){
+            // if((l!=0 &&l>INT_MAX/l) ||( r!=0 && r>INT_MAX/r)) return false;
+             long mid = l*l + r*r;
+            if(mid == c) return true;
+            else if(mid<c) l++;
+            else r--;
         }
         return false;
+
+        // int a = sqrt(c);
+        // for(long i=0;i*i<= c;i++){
+        //     double b = sqrt(c - i*i);
+        //     if(b == (int)b ) return true;
+        // }
+        // return false;
     }
 };
