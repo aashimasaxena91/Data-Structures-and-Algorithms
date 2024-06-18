31263c81e7c1da3d9e2b6dc3aebb0f6572d37849
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-06-18 22:33:00 +0530

    Time: 31 ms (40.87%), Space: 30.5 MB (99.44%) - LeetHub

--- 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
+++ 0453-minimum-moves-to-equal-array-elements/0453-minimum-moves-to-equal-array-elements.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int minMoves(vector<int>& nums) {
-        int n= nums.size();
-        sort(nums.begin(), nums.end());
+        // int n= nums.size();
+        // sort(nums.begin(), nums.end());
         int minStep=0;
-        for(int i=n-1;i>0;i--){
-           nums[i] +=minStep;
-           int temp = nums[i]- nums[0];
-            nums[0] = nums[i];
-            minStep+=temp;
-
-        }
+        int mn = INT_MAX;
+        for(int x: nums) mn = min(mn, x);
+        for(int x:nums) minStep+= x-mn;
         return minStep;
     }
 };
