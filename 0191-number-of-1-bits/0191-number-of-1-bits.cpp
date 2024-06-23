f5c6cc6b951ae33e03b052e541bf8d3c982031c2
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-06-23 18:40:14 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (66.42%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -3,9 +3,10 @@ public:
     int hammingWeight(int n) {
         int c =0;
         while(n!=0){
-           int k= n&1;
+            
+           int k= n%2;
             c+=k;
-          n=  n>>1;
+          n=  n/2;
         }
         return c;
     }
