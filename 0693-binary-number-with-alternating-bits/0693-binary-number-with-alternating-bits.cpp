7907522c38fc386a46557bffba5cc18880c98fce
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-06-23 19:24:56 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (30.06%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.cpp
@@ -1,16 +1,9 @@
 class Solution {
 public:
     bool hasAlternatingBits(int n) {
-       int c=0;
-        if(n%2==0)
-         c= 1;
-        else c=0;
-        while(n!=0){
-            int k = n%2;
-            if(k==c) return false;
-            c=k;
-            n=n/2;
-        }
-        return true;
+        long long int k = n^(n>>1);
+        if(((k+1)&k)==0) return true;
+         else return false;
+        
     }
 };
