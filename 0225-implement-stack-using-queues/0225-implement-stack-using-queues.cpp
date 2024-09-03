bf936cc88c5eb38e8ce51fbf42852db18d0100a9
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-09-03 23:45:02 +0530

    Time: 2 ms (38.96%), Space: 8.6 MB (26.86%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,6 +1,6 @@
 class MyStack {
 public:
-    queue<int> q1, q2;
+    queue<int> q1;
     MyStack() {
         
     }
@@ -10,16 +10,16 @@ public:
     }
     
     int pop() {
-        while(q1.size()!=1){
-            q2.push(q1.front());
+        int w = q1.size();
+        while(w>1){
+            w--;
+            q1.push(q1.front());
             q1.pop();
         }
+
         int t = q1.front();
         q1.pop();
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
-        }
+
         return t;
     }
     
