1568dd52095c71f2619b24672fd6baa5758a8cca
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-09-06 21:11:22 +0530

    Time: 4 ms (47.81%), Space: 10 MB (36.91%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
@@ -2,14 +2,23 @@ class Solution {
 public:
 
     unordered_map<string, string> mp;
+    string chars = "0123456789abcdefghijklmnopqrstuvwxyzABCHEFGHIJKLMNOPQRSTUVWXYZ";
     int num=0;
     // Encodes a URL to a shortened URL.
+    
+    string getCode(){
+        string c="";
+        for(int i=0;i<6;i++){
+            c+=chars[rand()%62];
+        }
+        c= "http://tinyurl.com/" + c;
+        return c;
+    }
+    
     string encode(string longUrl) {
-        string k = to_string(num);
-        num++;
-        string q  = "https://tinyurl.com/" + k;
-        mp[q] = longUrl;
-        return q;
+       string code = getCode();
+        mp[code] = longUrl;
+        return code;
     }
 
     // Decodes a shortened URL to its original URL.
