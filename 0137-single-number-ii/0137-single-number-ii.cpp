a73abc13e5cdeed61d716c0ac8a630506dc8a4bd
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-09-07 22:12:03 +0530

    Time: 4 ms (79.28%), Space: 12.1 MB (55.07%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -1,20 +1,11 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        int aloneBit =0;
-        
-        for(int i=0;i<32;i++){
-            int bitSum=0;
-            for(int k=0;k<nums.size();k++){
-                if((nums[k]>> i) & 1){
-                    bitSum++;
-                }
-            }
-            int t=0;
-            if(bitSum%3==1){
-                aloneBit = aloneBit | (1<<i);
-            }
+        int ones =0, twos=0;
+        for(int i=0;i<nums.size();i++){
+            ones = (ones^nums[i]) & (~twos);
+            twos = (twos^nums[i]) & (~ones);
         }
-        return aloneBit;
+        return ones;
     }
 };
