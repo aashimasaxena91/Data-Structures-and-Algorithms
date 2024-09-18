dbe0b96078370c9e4a28ba2006522e3cfe244475
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-09-18 17:53:54 +0530

    Time: 5 ms (28.87%), Space: 10.6 MB (46.65%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.cpp
@@ -10,36 +10,30 @@
  */
 class Solution {
 public:
-    ListNode* reve(ListNode* head, ListNode* end, ListNode* prev){
-        ListNode* cur = head;
-        ListNode* d = prev;
-        while(cur!=NULL && cur!=d){
-            head = head->next;
-            cur->next =prev;
-            prev = cur;
-            cur = head;
-        }
-        return prev;
-    }
     ListNode* reverseBetween(ListNode* head, int left, int right) {
-        if(left == right || head->next == NULL ||!head) return head;
-        int k=1;
-        ListNode* cur=head, * l, *r;
-        if(left == 1) l =head;
-        while(cur->next!=NULL){
-            if(k == left-1){
-                l = cur;
-            }
-            if(k == right){
-                r = cur;
-                break;
-            }
-            k++;
+        if(!head || !head->next || left == right) return head;
+        
+        ListNode* dum = new ListNode(-1);
+        ListNode* cur = head, *prev = dum;
+        
+        dum->next = head;
+        while(left>1){
+            left--;
+            prev = cur;
             cur=cur->next;
+            right--;
         }
-        if(left == 1)
-            return reve(head, r, r->next);
-        l->next = reve(l->next, r, r->next);
-        return head;
+        ListNode* tail = cur, *j = cur, *t = NULL;
+        while(right>0 && cur!=NULL){
+            right--;
+            tail = tail->next;
+            cur->next = t;
+            t= cur;
+            cur=tail;
+        }
+        prev->next = t;
+        j->next = cur;
+        return dum->next;
+        
     }
 };
