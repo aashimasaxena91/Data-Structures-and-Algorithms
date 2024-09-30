64b2f92843feed4ed702458f4ac90dfa45bec0d2
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-09-30 17:21:59 +0530

    Time: 7 ms (51.75%), Space: 17.2 MB (40.22%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,24 +1,27 @@
 class Solution {
 public:
-    
-    void soln(vector<vector<int>>& image,int m, int n, int color, int i, int j,vector<int> & x,vector<int>& y, int oldC){           if(i<0 || i>=m || j<0 || j>=n  ) return;
-        if(image[i][j]!= oldC) return;
-        image[i][j] = color;
-        for(int k =0;k<4;k++){ 
-            soln(image,m, n, color, i+x[k],j+y[k], x,y, oldC);
-        }
-    }
-    
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int m = image.size(),  n = image[0].size();
         
-        int m = image.size();
-        int n = image[0].size();
-        int oldC = image[sr][sc];
-        if(oldC== color) return image;
-        vector<int> x = {-1,1,0,0};
-        vector<int> y = {0, 0, -1,1};
-        
-        soln(image,m, n, color,sr,sc, x, y , oldC);
+        int k = image[sr][sc];
+        if(k==color) return image;
+        queue<pair<int, int>> q;
+        image[sr][sc] = color;
+        q.push({sr,sc});
+        vector<int> x = {0,0,-1,1}, y ={1,-1,0,0};
+        while(!q.empty()){
+            auto t = q.front();
+            q.pop();
+            int i = t.first;
+            int j = t.second;
+            for(int r =0;r<4;r++){
+                int iR = i+x[r], jR = j+ y[r];
+                if(iR<0 || iR>=m || jR<0 || jR>=n || image[iR][jR]!=k) continue;
+                image[iR][jR] = color;
+                q.push({iR, jR});
+            }
+            
+        }
         return image;
     }
 };
