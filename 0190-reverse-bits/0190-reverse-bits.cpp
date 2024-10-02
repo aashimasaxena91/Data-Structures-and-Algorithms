c25c1fb47ff14635766e594c6daccdd84969f15e
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-10-02 17:03:44 +0530

    Time: 4 ms (35.09%), Space: 7.4 MB (68.07%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
-        int res=0;
+        uint32_t ans =0;
         for(int i=0;i<32;i++){
-            int lsb = n&1;
-            int revL = lsb<<(31-i);
-            res = res | revL;
-            n=n>>1;
+            ans= ans<<1;
+            ans|=(n&1);
+           n= n>>1;
+           
         }
-        return res;
+        return ans;
     }
 };
