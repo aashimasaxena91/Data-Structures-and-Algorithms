438415775263c07036cf3071701eb25f8f80f55f
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-10-02 13:53:36 +0530

    Time: 7 ms (15.87%), Space: 14 MB (52.44%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int l =0, r = nums.size()-1;
+        int n = nums.size();
+        int l=0, r= n-1;
         while(l<=r){
             int mid = l+(r-l)/2;
-            if(nums[mid] == target) return mid;
-            else if(nums[mid]>=nums[l]){
-                if( target >= nums[l] && target<nums[mid])
-                    r = mid-1;
+            if(nums[mid]== target) return mid;
+            if(nums[mid]> nums[r]){
+                if(target<nums[mid] && target>=nums[l])
+                 r = mid-1;
                 else
-                    l = mid+1;
+                     l=mid+1;
             }else{
-                 if( nums[r]>= target && nums[mid]< target)
+                if(target<=nums[r] && target>nums[mid])
                     l = mid+1;
                 else
-                    r = mid-1;
+                   
+                 r=mid-1;
             }
         }
         return -1;
