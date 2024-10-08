2b753fa61ac77b2b4f4cf15636e0cb06087a6d54
Aashima Saxena <70135562+aashimasaxena91@users.noreply.github.com>
2024-10-08 19:28:21 +0530

    Time: 37 ms (41.32%), Space: 67.3 MB (50.36%) - LeetHub

--- 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.cpp
+++ 2956-find-common-elements-between-two-arrays/2956-find-common-elements-between-two-arrays.cpp
@@ -1,26 +1,18 @@
 class Solution {
 public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
-        int a =0, b=0;
-        sort(nums1.begin(), nums1.end());
-        sort(nums2.begin(), nums2.end());
-        int i=0, j=0;
-        while(i<nums1.size() && j<nums2.size()){
-            if(nums1[i]==nums2[j]){
-                i++;
-                j++;
-               
-                int l=0, r=0;
-                while(i<nums1.size() && nums1[i]==nums1[i-1]){i++; l++;}
-                 while(j<nums2.size() && nums2[j]==nums2[j-1]){j++; r++;}
-                a=a+l+1;
-                b = b+r+1;
-            }else if(nums1[i]<nums2[j]){
-                i++;
-            }else{
-                j++;
+        int a=0, b=0;
+        unordered_map<int, int> mp;
+        for(auto k: nums1){
+            mp[k]++;
+        }
+        for(auto t:nums2){
+            if(mp.find(t)!=mp.end()){
+                a+=mp[t];
+                mp[t]=0;
+                b++;
             }
         }
-        return {a, b};
+        return {a,b};
     }
 };
